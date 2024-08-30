74a297d12708785663d62f02584bf24c67b67546
Maxi <maxi.gatto96@gmail.com>
2024-08-30 15:55:05 -0300

    refactor: contact interfaces

--- src/interfaces/contact.interfaces.ts
+++ src/interfaces/contact.interfaces.ts
@@ -1,36 +1,4 @@
-import { Address, Phone } from '../entities';
 import { PhoneTypeEnum } from '../entities/phoneType.entity';
-
-export interface ICreateContact {
-  firstName: string;
-  lastName: string;
-  dateOfBirth: string;
-  email: string;
-  phones: Phone[];
-  addresses: Address[];
-}
-
-export interface IGetContact {
-  id?: number;
-  firstName?: string;
-  lastName?: string;
-  dateOfBirth?: string;
-  email?: string;
-  phoneNumber?: string;
-  phoneType?: PhoneTypeEnum;
-  locality?: string;
-  street?: string;
-  number?: number;
-}
-
-export interface IUpdateContact {
-  firstName?: string;
-  lastName?: string;
-  dateOfBirth?: string;
-  email?: string;
-  phones?: Phone[];
-  addresses?: Address[];
-}
 
 interface IPlainPhoneType {
   typeName: PhoneTypeEnum;
@@ -48,11 +16,37 @@ interface IPlainAddress {
   notes?: string;
 }
 
-export interface IPlainContact {
+export interface ICreateContact {
   firstName: string;
   lastName: string;
   dateOfBirth: string;
   email: string;
   phones: IPlainPhone[];
   addresses: IPlainAddress[];
+}
+
+export interface IGetContact {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+  email?: string;
+  phoneNumber?: string;
+  phoneType?: PhoneTypeEnum;
+  locality?: string;
+  street?: string;
+  number?: number;
+}
+
+export interface IPlainContact extends ICreateContact {
+  id: number;
+}
+
+export interface IUpdateContact {
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+  email?: string;
+  phones?: IPlainPhone[];
+  addresses?: IPlainAddress[];
 }
